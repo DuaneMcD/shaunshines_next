741ff615e4706d5fdf9797b763e8b20235097bf7
Duane McDonald <mcdonald.duane13@gmail.com>
2023-05-17 12:03:10 -0400

    updated address and link

--- components/Navbar.js
+++ components/Navbar.js
@@ -27,13 +27,8 @@ export function Navbar() {
             priority
           />
         </Link>
-        <a
-          id='address'
-          href='https://www.google.com/maps/place/Shaun+Shines+Automotive+Detailing/@36.8141086,-76.1014505,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xd628d231aa7bdf57!8m2!3d36.8141086!4d-76.0992618?authuser=5'
-          target='_blank'
-          rel='noreferrer'>
-          <i className='bi bi-geo-alt-fill'></i> 1397 Taylor Farm Road, Virginia
-          Beach, VA 23452
+        <a id='address' href='#' target='_blank' rel='noreferrer'>
+          <i className='bi bi-geo-alt-fill'></i> New Location Coming Soon!
         </a>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
